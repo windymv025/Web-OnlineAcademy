4860e06c9bc0de1189ad60dca422363af7f5fa7f
windymv025 <windymv025@users.noreply.github.com>
2021-01-17 17:24:10 +0700

    cài đặt chức năng tìm kiếm: view search, user.router.js, SearchByTittle in course.model.js

--- app.js
+++ app.js
@@ -1,146 +1,145 @@
-var express = require('express');
+var express = require("express");
 
-var createError = require('http-errors');
-var handlebars = require('express-handlebars');
-var hbs_sections = require('express-handlebars-sections');
+var createError = require("http-errors");
+var handlebars = require("express-handlebars");
+var hbs_sections = require("express-handlebars-sections");
 
-var path = require('path');
-var morgan = require('morgan');
-var Handlebars = require('handlebars');
+var path = require("path");
+var morgan = require("morgan");
+var Handlebars = require("handlebars");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin/admin.route')
-var accountRouter = require('./routes/account/account.route')
-var categoryRouter = require('./routes/category/category.route')
-const viewEngine = require('./middleware/viewEngine')
-const session = require('./middleware/session')
-const passport = require('./middleware/passport')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/user/user.route");
+var adminRouter = require("./routes/admin/admin.route");
+var accountRouter = require("./routes/account/account.route");
+var categoryRouter = require("./routes/category/category.route");
+const viewEngine = require("./middleware/viewEngine");
+const session = require("./middleware/session");
+const passport = require("./middleware/passport");
 
 var app = express();
 
+viewEngine(app);
+app.set("views", path.join(__dirname, "views"));
+session(app);
+passport(app);
 
-viewEngine(app)
-app.set('views', path.join(__dirname, 'views'));
-session(app)
-passport(app)
-
-app.use(require('./middleware/auth.local'));
+app.use(require("./middleware/auth.local"));
 var server = app.listen(5000, function () {
-    var host = server.address().address == '::' ? '127.0.0.1' : server.address().address
-    var port = server.address().port
-    console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s", host, port)
+  var host =
+    server.address().address == "::" ? "127.0.0.1" : server.address().address;
+  var port = server.address().port;
+  console.log(
+    "Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s",
+    host,
+    port
+  );
 });
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 //App route
-app.get('/', function (req, res, next) {
-    if (req.isAuthenticated()) {
-        res.redirect('admin');
-    } else {
-        res.redirect('/account/login')
-    }
+app.get("/", function (req, res, next) {
+  if (req.isAuthenticated()) {
+    res.redirect("admin");
+  } else {
+    res.redirect('/account/login')
+    // res.render("home", {
+    //   layout: false,
+    // });
+  }
 });
-app.use('/admin', adminRouter);
-app.use('/account', accountRouter);
-app.use('/category', categoryRouter);
-
-
-
+app.use("/admin", adminRouter);
+app.use("/account", accountRouter);
+app.use("/category", categoryRouter);
+app.use("/courses", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+   next(createError(404));
 });
 
 app.use(function (req, res) {
-    res.render('404', {
-        layout: false
-    })
+  res.render("404", {
+    layout: false,
+  });
 });
 // default error handler
 app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.render('500', {
-        layout: false
-    })
-})
+  console.error(err.stack);
+  res.render("500", {
+    layout: false,
+  });
+});
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    // res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
 });
 
-Handlebars.registerHelper('page', function (n, block) {
-    var accum = '';
-    for (var i = 1; i <= n; ++i)
-        accum += block.fn(i);
-    return accum;
+Handlebars.registerHelper("page", function (n, block) {
+  var accum = "";
+  for (var i = 1; i <= n; ++i) accum += block.fn(i);
+  return accum;
 });
 
-Handlebars.registerHelper('for', function (from, to, incr, block) {
-    var accum = '';
-    for (var i = from; i < to; i += incr)
-        accum += block.fn(i);
-    return accum;
+Handlebars.registerHelper("for", function (from, to, incr, block) {
+  var accum = "";
+  for (var i = from; i < to; i += incr) accum += block.fn(i);
+  return accum;
 });
 
-Handlebars.registerHelper('prevPage', function (from, to, incr, block) {
-    var accum = '';
-    for (var i = from; i < to; i += incr)
-        accum += block.fn(i);
-    return accum;
+Handlebars.registerHelper("prevPage", function (from, to, incr, block) {
+  var accum = "";
+  for (var i = from; i < to; i += incr) accum += block.fn(i);
+  return accum;
 });
 
-Handlebars.registerHelper('nextPage', function (from, to, incr, block) {
-    var accum = '';
-    for (var i = from + 1; i <= to; i += incr)
-        accum += block.fn(i);
-    return accum;
+Handlebars.registerHelper("nextPage", function (from, to, incr, block) {
+  var accum = "";
+  for (var i = from + 1; i <= to; i += incr) accum += block.fn(i);
+  return accum;
 });
 
-Handlebars.registerHelper('ifCond', function (v1, v2, options) {
-    if (v1 === v2) {
-        return options.fn(this);
-    }
-    return options.inverse(this);
+Handlebars.registerHelper("ifCond", function (v1, v2, options) {
+  if (v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
 });
-Handlebars.registerHelper('ifMoreCond', function (v1, operator, v2, options) {
-
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-            return (v1 != v2) ? options.fn(this) : options.inverse(this);
-        case '!==':
-            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-        default:
-            return options.inverse(this);
-    }
+Handlebars.registerHelper("ifMoreCond", function (v1, operator, v2, options) {
+  switch (operator) {
+    case "==":
+      return v1 == v2 ? options.fn(this) : options.inverse(this);
+    case "===":
+      return v1 === v2 ? options.fn(this) : options.inverse(this);
+    case "!=":
+      return v1 != v2 ? options.fn(this) : options.inverse(this);
+    case "!==":
+      return v1 !== v2 ? options.fn(this) : options.inverse(this);
+    case "<":
+      return v1 < v2 ? options.fn(this) : options.inverse(this);
+    case "<=":
+      return v1 <= v2 ? options.fn(this) : options.inverse(this);
+    case ">":
+      return v1 > v2 ? options.fn(this) : options.inverse(this);
+    case ">=":
+      return v1 >= v2 ? options.fn(this) : options.inverse(this);
+    case "&&":
+      return v1 && v2 ? options.fn(this) : options.inverse(this);
+    case "||":
+      return v1 || v2 ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
+  }
 });
 
 module.exports = app;
--- models/course.model.js
+++ models/course.model.js
@@ -19,6 +19,7 @@ module.exports = {
         }
         return db.load(sql);
     },
+    
     async count() {
         let sql = `select count(*) count from ${TBL_COURSE} cs join 
         category c on c.id = cs.category_id and c.status = 1
@@ -39,4 +40,18 @@ module.exports = {
         delete entity.CatID;
         return db.patch(entity, condition, TBL_COURSE);
     }
+}
+
+module.exports.searchByTitle = async (keyword)=>{
+// (keyword ,page, pageSize, orderBy = 'cs.created_at asc') => {
+        let sql = `select cs.*, c.name catName, u.name createdName from ${TBL_COURSE} cs join 
+        category c on c.id = cs.category_id and c.status = 1
+        left join users u on u.id = cs.created_by and u.status = 1
+        where cs.status = 1 and match(title) against('${keyword}' with query expansion)`
+        // if (page != null) {
+        //     let offset = Number(page) * Number(pageSize)
+        //     sql = sql + ` limit ${pageSize} offset ${offset} `
+        // }
+        return await db.load(sql);
+    // },
 }
